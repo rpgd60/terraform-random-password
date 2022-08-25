40c5d0f06f87570b3fbf9334a34146412ec4717d
Rafa <rafa@portillo.eu>
2022-08-25 04:52:20 +0200

    new label

--- examples/simple_password/main.tf
+++ examples/simple_password/main.tf
@@ -1,7 +1,7 @@
 
 module "a_password" {
   # source = "cloudposse/ssh-key-pair/tls"
-  source = "github.com/rpgd60/terraform-random-password.git?ref=v0.0.2"
+  source = "github.com/rpgd60/terraform-random-password.git?ref=v0.0.3"
 
   length = 12
 }
